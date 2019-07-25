f0c4c8ea5cb7e9c556913d7c10c7f7b61899b7ae
mliksunov <mliksunov@magecom.us>
2019-07-25 11:44:23 +0300

    redux add data

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,6 @@ import * as type from './constants';
 
 const createAction = type => payload => ({type, payload});
 
-const sidebarToggle = createAction(type.SIDEBAR_TOGGLE);
-const addCategory = createAction(type.CATEGORY_ADD);
-const togglePopup = createAction(type.POPUP_TOGGLE);
-
-export {sidebarToggle, addCategory, togglePopup};
+export const sidebarToggle = createAction(type.SIDEBAR_TOGGLE);
+export const addCategory = createAction(type.CATEGORY_ADD);
+export const togglePopup = createAction(type.POPUP_TOGGLE);
